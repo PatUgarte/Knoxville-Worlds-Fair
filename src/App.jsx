e80e4f0505c26ce6ba7fd56e1db962223419d58a
Pat Ugarte <patriciomugarte@gmail.com>
2020-07-13 19:13:11 -0300

    Initial configuration and Hello World

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+    return (
+        <h1>
+            Hola, Feria Mundial de Knoxville - Tennesee
+        </h1>
+    );
+}
+
+export default App;
